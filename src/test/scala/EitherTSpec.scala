1ca4bc692e2ff18c1e9f10149582e174104fca86
samir garg <szgarg@linklaters.com>
2021-06-16 10:58:10 +0100

    Add flattap

--- src/test/scala/EitherTSpec.scala
+++ src/test/scala/EitherTSpec.scala
@@ -16,6 +16,18 @@ class EitherTSpec extends AnyWordSpec with Matchers {
         changedRightET.value shouldBe Success(Right("1"))
       }
     }
+
+    /**
+     * The typical use case for .tap is that you want to perform an operation which returned value can be discarded safely,
+     * without breaking your logic. Usually, it's Unit returned by immediately side-effecting operation:
+     */
+    "semiFlatTap" should {
+      "perform a side affecting operation and return the original result" in {
+        val either: Right[Throwable, Int] = Right[Throwable, Int](1)
+        val et: EitherT[Try, Throwable, Int] = EitherT[Try, Throwable, Int](Try(either))
+        et.semiflatTap(i => Try(i.toString)) shouldBe et
+      }
+    }
   }
 
 }
